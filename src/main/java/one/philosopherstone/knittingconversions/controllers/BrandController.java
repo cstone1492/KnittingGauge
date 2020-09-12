0904fbead55ac13d5b292fd9fdb7c246f96fed4e
Caroline STone <docphilstone@gmail.com>
2020-09-12 13:17:50 -0500

    adding delete page for brands

--- src/main/java/one/philosopherstone/knittingconversions/controllers/BrandController.java
+++ src/main/java/one/philosopherstone/knittingconversions/controllers/BrandController.java
@@ -5,10 +5,7 @@ import one.philosopherstone.knittingconversions.models.data.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("brand")
@@ -36,4 +33,24 @@ public class BrandController {
 
         return "brand/index";
     }
+
+    @GetMapping("delete")
+    public String displayDeleteBrandForm(Model model) {
+        model.addAttribute("brands", brandRepository.findAll());
+
+        return "brand/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteBrandForm(@RequestParam(required = false)int[] brandIds){
+
+        if (brandIds != null) {
+            for (int id : brandIds) {
+                brandRepository.deleteById(id);
+            }
+        }
+
+        return "brand/index";
+    }
+
 }
--- src/main/java/one/philosopherstone/knittingconversions/controllers/ColorController.java
+++ src/main/java/one/philosopherstone/knittingconversions/controllers/ColorController.java
@@ -1,4 +1,39 @@
 package one.philosopherstone.knittingconversions.controllers;
 
+import one.philosopherstone.knittingconversions.models.Color;
+import one.philosopherstone.knittingconversions.models.data.ColorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("color")
 public class ColorController {
+
+    @Autowired
+    private ColorRepository colorRepository;
+
+    @GetMapping("add")
+    public String displayAddColorForm(Model model) {
+        model.addAttribute(new Color());
+        return ("color/add");
+    }
+
+    @PostMapping("add")
+    public String processAddColorForm(@ModelAttribute Color newColor, Model model) {
+        colorRepository.save(newColor);
+        return "redirect:";
+    }
+
+    @GetMapping("")
+    public String displayColors(Model model) {
+        model.addAttribute("title", "All Colors");
+        model.addAttribute("colors", colorRepository.findAll());
+        return "color/index";
+    }
+
 }
